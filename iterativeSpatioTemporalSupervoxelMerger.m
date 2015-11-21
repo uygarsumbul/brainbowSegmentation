c4cf37de800b362ca037c82902fa1696217733d4
uygarsumbul <uygar@stat.columbia.edu>
2015-11-21 18:13:49 -0500

    some refinement to the iterative approach

--- iterativeSpatioTemporalSupervoxelMerger.m
+++ iterativeSpatioTemporalSupervoxelMerger.m
@@ -1,4 +1,4 @@
-function [svMeans, svCells, square_sAff] = iterativeSpatioTemporalSupervoxelMerger(svMeans, svCells, square_sAff, stackSize, opts_mCCWIC, opts_fkmeans)
+function [svMeans, svCells, square_sAff] = iterativeSpatioTemporalSupervoxelMerger(svMeans, svCells, square_sAff, stackSize, superVoxelOpts, opts_mCCWIC, opts_fkmeans)
 
 cc                                             = size(svMeans, 1);
 ccOLD                                          = 2*cc;
@@ -7,13 +7,19 @@ for kk = 1:cc
   voxelCounts(kk)                              = numel(svCells{kk});
 end
 nhoods                                         = [6 18 26];
-nhood                                          = 6;
 nhoodPointer                                   = 1;
-flag                                           = false;
+nhood                                          = nhoods(nhoodPointer);
+nhoodFlag                                      = false;
 ccLog                                          = cc;
 osFactorLog                                    = opts_mCCWIC.overSegmentingFactor;
+if strcmp(opts_mCCWIC.condition, 'decay')
+  loopFlag                                     = (ccOLD~=cc);
+else
+  loopFlag                                     = (opts_mCCWIC.overSegmentingFactor>opts_mCCWIC.minOverSegmentingFactor);
+end
+normFlag                                       = true;
 
-while opts_mCCWIC.overSegmentingFactor>opts_mCCWIC.minOverSegmentingFactor & ccOLD~=cc
+while loopFlag
   svMeansNorm                                  = svMeans ./ repmat(sqrt(sum(svMeans.^2, 2)), 1, size(svMeans, 2));
   if size(svMeans,2)==3
     svMeansLUV                                 = rgb2luv(svMeans')';
@@ -24,26 +30,37 @@ while opts_mCCWIC.overSegmentingFactor>opts_mCCWIC.minOverSegmentingFactor & ccO
     svMeansLUV(:,4)                            = svMeans(:,4)*50;
     svMeansNormLUV(:,4)                        = svMeansNorm(:,4)*50;
   end
-  index                                        = snrAwareKmeans(round(opts_mCCWIC.overSegmentingFactor*opts_mCCWIC.K), svMeansNormLUV, opts_fkmeans);
+  if normFlag
+    index                                      = snrAwareKmeans(round(opts_mCCWIC.overSegmentingFactor*opts_mCCWIC.K), svMeansNormLUV, opts_fkmeans);
+  else
+    index                                      = snrAwareKmeans(round(opts_mCCWIC.overSegmentingFactor*opts_mCCWIC.K), svMeansLUV, opts_fkmeans);
+  end
   [square_sAff, svMeans, svCells, voxelCounts] = mergeConnectedComponentsWithinIndividualClusters(index, square_sAff, svMeans, svCells, voxelCounts, opts_mCCWIC, nhood);
   opts_fkmeans.weight                          = voxelCounts;
   ccOLD                                        = cc;
   cc                                           = numel(svCells);
   ccLog(end+1)                                 = cc;
   osFactorLog(end+1)                           = opts_mCCWIC.overSegmentingFactor;
+  disp([cc nhood opts_mCCWIC.overSegmentingFactor nhoodFlag normFlag])
   if ccOLD/cc < opts_mCCWIC.fastRatio
-    if flag
+    if nhoodFlag
       opts_mCCWIC.overSegmentingFactor         = opts_mCCWIC.overSegmentingFactor * opts_mCCWIC.decayFactor;
-      flag                                     = false;
+      nhoodFlag                                = false;
     end
-    nhoodPointer                               = rem(nhoodPointer, numel(nhoodPointer)) + 1;
-    nhood                                      = nhood(nhoodPointer);
-    if nhoodPointer == numel(nhoodPointer)
-      flag                                     = true;
+    nhoodPointer                               = rem(nhoodPointer, numel(nhoods)) + 1;
+    nhood                                      = nhoods(nhoodPointer);
+    if nhoodPointer == numel(nhoods)
+      nhoodFlag                                = true;
     end
   end
-  disp(cc)
+  if strcmp(opts_mCCWIC.condition, 'decay')
+    loopFlag                                   = (ccOLD~=cc);
+  else
+    loopFlag                                   = (opts_mCCWIC.overSegmentingFactor>opts_mCCWIC.minOverSegmentingFactor);
+  end
+  normFlag                                     = ~normFlag;
   if rem(numel(ccLog), 10) == 0
     save -v7.3 data/iSTSM_sv_dcai2_sigma2000_hmin26_0.008_26n_split5e-11_sCR_aff.mat ccLog osFactorLog index opts_fkmeans opts_mCCWIC square_sAff stackSize superVoxelOpts svCells svMeans voxelCounts
   end
 end
+save -v7.3 data/iSTSM_sv_dcai2_sigma2000_hmin26_0.008_26n_split5e-11_sCR_aff.mat ccLog osFactorLog index opts_fkmeans opts_mCCWIC square_sAff stackSize superVoxelOpts svCells svMeans voxelCounts
