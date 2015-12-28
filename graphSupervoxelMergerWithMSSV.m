5cc565d8fc3fc1da5a3785d957e0a9fc55a4ac13
uygarsumbul <uygar@stat.columbia.edu>
2015-12-27 20:30:03 -0500

    more involved iteration

---
+++ graphSupervoxelMergerWithMSSV.m
@@ -0,0 +1,46 @@
+function [svMeans, svCells, square_sAff, origIndex] = graphSupervoxelMergerWithMSSV(svMeans, origsvMeansLUV, origIndex, svCells, square_sAff, opts)
+
+% opts. -- fkmeans, superVoxelOpts, stackSize, maxOverSegmentingFactor, minOverSegmentingFactor, maxOsFactorForSC, decayFactor, maxSubgraphSize, maxColorDifferenceRatio, disconnectedSVsizeTh
+osFactor                                                     = opts.maxOverSegmentingFactor;
+voxelCounts                                                  = cellfun(@numel, svCells)';
+ccLog                                                        = size(svMeans, 1);
+osFactorLog                                                  = osFactor;
+origDisconnectedSVsizeTh                                     = opts.disconnectedSVsizeTh;
+while osFactor>opts.minOverSegmentingFactor
+  % MERGE BASED ON SPACE-COLOR PROXIMITY
+  subgraphNodes                                              = randomlyPartitionBinaryGraph(square_sAff > 1/(sqrt(3)+1e-4), opts.maxSubgraphSize);
+  subgraph_svCells                                           = cell(1, numel(subgraphNodes));
+  subgraph_indices                                           = cell(1, numel(subgraphNodes));
+  nextOsFactor                                               = opts.outerDecayFactor*osFactor;
+  if size(svMeans,2)==3
+    svMeansLUV                                               = rgb2luv(svMeans')';
+  else
+    svMeansLUV                                               = [rgb2luv(svMeans(:,1:3)')' svMeans(:,4)*50];
+  end
+  parfor sg = 1:numel(subgraphNodes)
+    nn                                                       = subgraphNodes{sg};
+    K_vdpgm                                                  = vdpgm_countEstimate(origsvMeansLUV(ismember(origIndex, nn), :));
+    [sg_svCells, sg_svIndex]                                 = scMergerOnSubgraph(osFactor, nextOsFactor, K_vdpgm, opts, square_sAff(nn, nn), svMeans(nn, :), svCells(nn), voxelCounts(nn));
+    subgraph_svCells{sg}                                     = sg_svCells;
+    subgraph_indices{sg}                                     = sg_svIndex;
+  end
+  [square_sAff, svMeans, svCells, voxelCounts, idxTransform] = mergeSubgraphs2(square_sAff, svMeans, voxelCounts, opts.disconnectedSVsizeTh, subgraph_svCells, subgraph_indices, subgraphNodes);
+  origIndex                                                  = idxTransform(origIndex);
+  ccLog(end+1)                                               = numel(svCells);
+  osFactorLog(end+1)                                         = osFactor;
+  disp([ccLog(end) osFactorLog(end)])
+  % MERGE BASED ON SIZE
+  opts.disconnectedSVsizeTh                                  = round(mean(voxelCounts)/10);
+  opts.luvColorDistanceUpperBound                            = 300/opts.disconnectedSVsizeTh;
+  [square_sAff, svMeans, svCells, voxelCounts, origIndex]    = mergeSmallSuperVoxels(square_sAff, svMeans, svCells, voxelCounts, origIndex, opts);
+  opts.disconnectedSVsizeTh                                  = origDisconnectedSVsizeTh;
+  ccLog(end+1)                                               = numel(svCells);
+  osFactorLog(end+1)                                         = osFactor;
+  disp([ccLog(end) osFactorLog(end)])
+  % UPDATE FOR NEXT ITERATION
+  osFactor                                                   = nextOsFactor;
+  opts.maxColorDistSizeProduct                               = min(opts.maxColorDistSizeProduct*opts.colorDistSizeProductMultiplier, opts.upperBoundForColorDistSizeProduct);
+  fileName                                                   = [opts.saveFileName '_iter' num2str(numel(ccLog)-1) '.mat'];
+  save(fileName, 'ccLog', 'osFactorLog', 'opts', 'square_sAff', 'svCells', 'svMeans', 'origsvMeansLUV', 'voxelCounts', 'origIndex', '-v7.3');
+end
+
---
+++ mergeConnComp4.m
@@ -0,0 +1,100 @@
+function [square_sAff, svMeans, svCells, voxelCounts, tmpC] = mergeConnComp4(index, square_sAff, svMeans, svCells, voxelCounts, opts, connTh, careful)
+
+svMeansNorm                             = svMeans ./ repmat(sqrt(sum(svMeans.^2, 2)), 1, size(svMeans, 2));
+if size(svMeans,2)==3
+  svMeansLUV                            = rgb2luv(svMeans')';
+  svMeansNormLUV                        = rgb2luv(svMeansNorm')';
+else
+  svMeansLUV(:,1:3)                     = rgb2luv(svMeans(:,1:3)')';
+  svMeansNormLUV(:,1:3)                 = rgb2luv(svMeansNorm(:,1:3)')';
+  svMeansLUV(:,4)                       = svMeans(:,4)*50;
+  svMeansNormLUV(:,4)                   = svMeansNorm(:,4)*50;
+end
+
+newsvCells                              = cell(0);
+binsaff                                 = (square_sAff > connTh);
+tmpC                                    = zeros(1, numel(svCells));
+idx                                     = 1;
+for kk = 1:max(index)
+  thisCluster                           = find(index==kk);
+  [S, C]                                = graphconncomp(binsaff(thisCluster, thisCluster));
+  for mm = 1:S
+    thisConnComp                        = thisCluster(C==mm);
+    ntcc                                = numel(thisConnComp);
+    if careful && ntcc>1
+      tmp                               = reshape(harmmean([kron(ones(ntcc,1),voxelCounts(thisConnComp)) kron(voxelCounts(thisConnComp),ones(ntcc,1))],2),ntcc,ntcc);
+
+%      mat1                              = (squareform(pdist(svMeansLUV(thisConnComp,:))) < opts.maxColorDistSizeProduct ./ tmp);
+      mat2                              = (squareform(pdist(svMeansNormLUV(thisConnComp,:))) < opts.maxColorDistSizeProduct ./ tmp);
+
+%      [Stmp, Ctmp]                      = graphconncomp(mat1 & mat2 & binsaff(thisConnComp, thisConnComp));
+      [Stmp, Ctmp]                      = graphconncomp(mat2 & binsaff(thisConnComp, thisConnComp));
+
+      for nn = 1:Stmp
+        thisConnCompTmp                 = thisConnComp(Ctmp==nn);
+        newsvCells{end+1}               = cell2mat(svCells(thisConnCompTmp)');
+        tmpC(thisConnCompTmp)           = idx;
+        idx                             = idx + 1;
+      end
+    else
+      newsvCells{end+1}                 = cell2mat(svCells(thisConnComp)');
+      tmpC(thisConnComp)                = idx;
+      idx                               = idx + 1;
+    end
+  end
+end
+
+S                                       = idx - 1;
+[row, col, val]                         = find(square_sAff);
+newRows                                 = tmpC(row);
+newCols                                 = tmpC(col);
+tmp                                     = find(newRows<=newCols);
+newRows(tmp)                            = [];
+newCols(tmp)                            = [];
+val(tmp)                                = [];
+newsvMeans                              = zeros(S, size(svMeans, 2));
+newvoxelCounts                          = zeros(S, 1);
+rrr                                     = zeros(size(row));
+ccc                                     = rrr;
+vvv                                     = rrr;
+hhh                                     = rrr;
+thisPos                                 = 1;
+clear row; clear col;
+for kk = 1:numel(newRows)
+  thisHash                              = newRows(kk)*(S+1) + newCols(kk);
+  pos                                   = find(thisHash==hhh);
+  if isempty(pos)
+    rrr(thisPos)                        = newRows(kk);
+    ccc(thisPos)                        = newCols(kk);
+    vvv(thisPos)                        = val(kk);
+    hhh(thisPos)                        = thisHash;
+    thisPos                             = thisPos + 1;
+  else
+    vvv(pos)                            = max(vvv(pos), val(kk));
+  end
+end
+clear val;
+unused                                  = find(rrr==0, 1);
+rrr(unused:end)                         = [];
+ccc(unused:end)                         = [];
+vvv(unused:end)                         = [];
+clear hhh;
+square_sAff                             = sparse(rrr, ccc, vvv, S, S);
+square_sAff                             = square_sAff + transpose(square_sAff);
+for kk = 1:S
+  newvoxelCounts(kk)                    = numel(newsvCells{kk});
+  thisGroup                             = find(tmpC==kk);
+  newsvMeans(kk, :)                     = voxelCounts(thisGroup)' * svMeans(thisGroup, :) / sum(voxelCounts(thisGroup));
+end
+svMeans                                 = newsvMeans;
+svCells                                 = newsvCells;
+voxelCounts                             = newvoxelCounts;
+
+binsaff                                 = (square_sAff>1/sqrt(3)-1e-5);
+binsaff                                 = (sum(binsaff,2)>0);
+tmp                                     = find(~binsaff & (voxelCounts<opts.disconnectedSVsizeTh));
+square_sAff(tmp, :)                     = [];
+square_sAff(:, tmp)                     = [];
+svMeans(tmp, :)                         = [];
+svCells(tmp)                            = [];
+voxelCounts(tmp)                        = [];
