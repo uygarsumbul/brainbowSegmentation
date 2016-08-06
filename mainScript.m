42945b2aa18acd1f1a6cbfa343ea7040583865ac
uygarsumbul <uygar@stat.columbia.edu>
2016-08-05 21:40:24 -0400

    minor fixes

--- mainScript.m
+++ mainScript.m
@@ -1,31 +1,31 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SUPERVOXEL GENERATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-superVoxelOpts.filePreamble                                              = '~/bb/data/dawen/supervoxels_dcai_PVhippo1_bm4d_sigma500_ws0.005_sigma2000_ws0.005_augmented';
+superVoxelOpts.filePreamble                                              = '~/bb/data/dawen/supervoxels_dcai_brainbow4channel_bm4d_sigma500_ws0.005_split5e-11_20_augmented'; % PVhippo1_bm4d_sigma500_ws0.005_sigma2000_ws0.005_split5e-11_20_augmented';
 superVoxelOpts.brightnessThreshold                                       = 0.05;
 superVoxelOpts.spatialDistanceCalculationOpts.upperBound                 = 2;
-superVoxelOpts.splitHardSVopts.detThreshold                              = 1e-10;
+superVoxelOpts.splitHardSVopts.detThreshold                              = 5e-11;
 superVoxelOpts.splitHardSVopts.connectivity                              = 26;
-superVoxelOpts.splitHardSVopts.subdivisionSizeThreshold                  = 50;
-superVoxelOpts.dataset                                                   = '~/bb/data/dcai_PVhippo1_bm4d_sigma500.mat';
+superVoxelOpts.splitHardSVopts.subdivisionSizeThreshold                  = 20;
+superVoxelOpts.dataset                                                   = '~/bb/data/dcai_brainbow4channel_bm4d_sigma500.mat'; % dcai_PVhippo1_bm4d_sigma500.mat';
 superVoxelOpts.HMINTH26                                                  = 0.005;
 supervoxelize(superVoxelOpts);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CONSERVATIVE MERGING OF SUPERVOXELS %%%%%%%%%%%%%%%%%%%%%%%%%
-mergeOpts.loadFilename                                                   = '/home/uygar/bb/data/dawen/supervoxels_dcai_PVhippo1_bm4d_sigma500_ws0.005_augmented6_subdivide50.mat';
-mergeOpts.saveFileName                                                   = '/home/uygar/bb/data/dawen/merged_supervoxels_dcai_PVhippo1_bm4d_sigma500_ws0.005_augmented6_subdivide50';
+mergeOpts.loadFilename                                                   = '/home/uygar/bb/data/dawen/supervoxels_dcai_brainbow4channel_bm4d_sigma500_ws0.005_split5e-11_20_augmented_aff.mat'; % PVhippo1_bm4d_sigma500_ws0.005_augmented6_subdivide50.mat';
+mergeOpts.saveFileName                                                   = '/home/uygar/bb/data/dawen/merged_supervoxels_dcai_brainbow4channel_bm4d_sigma500_ws0.005_split5e-11_20_augmented'; % PVhippo1_bm4d_sigma500_ws0.005_augmented6_subdivide50';
 mergeOpts.zAnisotropy                                                    = 3;
-mergeOpts.demix.maxSimilarNeighborNormLUVDist                            = 30 * sqrt(size(svMeans, 2)/4);
+mergeOpts.demix.maxSimilarNeighborNormLUVDist                            = 30; % * sqrt(size(svMeans, 2)/4);
 mergeOpts.demix.minImprovementFactor                                     = 3;
 mergeOpts.demix.maxSizeForDemixing                                       = 500;
 mergeOpts.mergeSmallSuperVoxels.luvColorDistanceUpperBound               = 30;
 mergeOpts.mergeSmallSuperVoxels.disconnectedSVsizeTh                     = 20;
 mergeOpts.mergeWRTnAo.sDist                                              = sqrt(3);
 mergeOpts.mergeWRTnAo.minDotProduct                                      = 1/sqrt(2);
-mergeOpts.mergeWRTnAo.maxColorDist                                       = 6 * sqrt(size(svMeans, 2)/4);
-mergeOpts.mergeCloseNeighborhoods.maxDistNormLUV                         = 6 * sqrt(size(svMeans, 2)/4);
+mergeOpts.mergeWRTnAo.maxColorDist                                       = 6; % * sqrt(size(svMeans, 2)/4);
+mergeOpts.mergeCloseNeighborhoods.maxDistNormLUV                         = 6; % * sqrt(size(svMeans, 2)/4);
 mergeOpts.kmeansMerging.clusterCount                                     = 40;
 mergeOpts.kmeansMerging.overClusteringFactor                             = 5;
-mergeOpts.kmeansMerging.maxColorDistance                                 = 6 * sqrt(size(svMeans, 2)/4);
-mergeSupervoxels(mergeOpts)
+mergeOpts.kmeansMerging.maxColorDistance                                 = 6; % * sqrt(size(svMeans, 2)/4);
+mergeSupervoxels(mergeOpts);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SEGMENTATION OF MERGED SUPERVOXELS %%%%%%%%%%%%%%%%%%%%%%%%%
 mergedSvFileName                    = '/home/uygar/bb/data/dawen/merged_supervoxels_dcai_PVhippo1_bm4d_sigma500_ws0.005_augmented6_subdivide50_sAff15.mat';
 graphData.c                         = 0.5e-3;
